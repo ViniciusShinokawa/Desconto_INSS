542efb0479fe499f5a51117816dcc2e9712b7daf
Vinicius Shinokawa <viniciusakutagawa24@gmail.com>
2024-06-14 16:09:21 -0300

    Adicionar arquivos de projeto.

---
+++ Desconto_INSS/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desconto_INSS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string cargo;
+            double salario, inss;
+
+            Console.WriteLine("Infomre seu cargo:");
+            cargo = Console.ReadLine();
+
+            Console.WriteLine("Informe seu salario: ");
+            salario = Convert.ToDouble(Console.ReadLine());
+
+            if (salario <= 3000) inss = salario * 10 / 100; 
+            else inss = salario * 14 / 100;
+
+            Console.Clear();
+
+            Console.WriteLine("Cargo: " + cargo);
+            Console.WriteLine("Salario: " + salario); 
+            Console.WriteLine("Valor INSS: " + inss);
+            Console.WriteLine("Salario liquido:  " +(salario - inss));
+
+            Console.ReadKey();
+        }
+    }
+}
